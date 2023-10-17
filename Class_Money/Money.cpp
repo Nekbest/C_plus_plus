f5c4c3c38499471684de1a26727bcf8ada0eced4
Nekbest <139134738+Nekbest@users.noreply.github.com>
2023-10-17 19:30:46 +0300

    Update Money.cpp

--- Class_Money/Money.cpp
+++ Class_Money/Money.cpp
@@ -1,5 +1,17 @@
 #include "Money.h"
 
+Money::Money()
+{
+	for (int i = 0; i < 8; ++i)
+	{
+		denominations_rubles[i] = 0;
+	}
+	for (int i = 0; i < 3; ++i)
+	{
+		denominations_pennies[i] = 0;
+	}
+}
+
 
 Money::Money(int* a)
 {
@@ -9,7 +21,7 @@ Money::Money(int* a)
 	}
 	for (int i = 0; i < 3; ++i)
 	{
-		denominations_pennies[i] = a[i+8];
+		denominations_pennies[i] = a[i + 8];
 	}
 }
 
@@ -50,15 +62,27 @@ float Money::difference_amount(float other_sum)
 }
 
 
-int Money::get_denomination_ruble(int index)
+int Money::get_denomination_rubles(int index) const
 {
 	return denominations_rubles[index];
 }
 
 
-int Money::get_denomination_pennie(int index)
+int Money:: get_denomination_pennies(int index) const
 {
 	return denominations_pennies[index];
+}
+
+
+int Money::get_nominal_rubles(int index) const
+{
+	return nominal_rubles[index];
+}
+
+
+float Money::get_nominal_pennies(int index) const
+{
+	return nominal_pennies[index];
 }
 
 
@@ -75,41 +99,60 @@ std::string Money::toString()
 }
 
 
-int Money::set_denomination_ruble(int index, int meaning)
+int Money::set_denomination_rubles(int index, int meaning)
 {
 	switch (index)
 	{
-		case 0:
-			denominations_rubles[0] = meaning;
-		case 1:
-			denominations_rubles[1] = meaning;
-		case 2:
-				denominations_rubles[2] = meaning;
-		case 3:
-				denominations_rubles[3] = meaning;
-		case 4:
-			denominations_rubles[4] = meaning;
-		case 5:
-			denominations_rubles[5] = meaning;
-		case 6:
-			denominations_rubles[6] = meaning;
-		case 7:
-			denominations_rubles[7] = meaning;
+	case 0:
+		denominations_rubles[0] += meaning;
+		break;
+	case 1:
+		denominations_rubles[1] += meaning;
+		break;
+	case 2:
+		denominations_rubles[2] += meaning;
+		break;
+	case 3:
+		denominations_rubles[3] += meaning;
+		break;
+	case 4:
+		denominations_rubles[4] += meaning;
+		break;
+	case 5:
+		denominations_rubles[5] += meaning;
+		break;
+	case 6:
+		denominations_rubles[6] += meaning;
+		break;
+	case 7:
+		denominations_rubles[7] += meaning;
 	}
 	return denominations_rubles[index];
 }
 
 
-int Money::set_denomination_pennie(int index, int meaning)
+int Money::set_denomination_pennies(int index, int meaning)
 {
 	switch (index)
 	{
 	case 0:
-		denominations_pennies[0] = meaning;
+		denominations_pennies[0] += meaning;
+		break;
 	case 1:
-		denominations_pennies[1] = meaning;
+		denominations_pennies[1] += meaning;
+		break;
 	case 2:
-		denominations_pennies[2] = meaning;
+		denominations_pennies[2] += meaning;
 	}
 	return denominations_pennies[index];
+}
+
+
+Money& Money::operator =(const Money& other_money)
+{
+	for (int i = 0; i < 8; i++)
+		denominations_rubles[i] =  other_money.get_denomination_rubles(i);
+	for (int i = 0; i < 3; ++i)
+		denominations_pennies[i] = other_money.get_denomination_pennies(i);
+	return *this;
 }
