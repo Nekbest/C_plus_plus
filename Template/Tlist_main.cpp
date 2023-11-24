92e379d37f8a40b2a62d2d7cfe857649605c8aa9
Nekbest <139134738+Nekbest@users.noreply.github.com>
2023-11-24 17:29:57 +0300

    Update Tlist_main.cpp

--- Template/Tlist_main.cpp
+++ Template/Tlist_main.cpp
@@ -4,33 +4,39 @@
 
 int main()
 {
-	int n = 20;
-	std::string* string_massive = new std::string [n];
-	for (int i = 0; i < n; i++)
-		string_massive[i] = std::to_string(i);
-	Tlist<std::string> list_1("- 1");
-	list_1.AddTail(string_massive, n);
-	std::cout << list_1;
-	std::cout << "____________________________________\n";
+	int* int_massive = new int [10];
+	for (int i = 0; i < 10; i++)
+		int_massive[i] = i;
+	Tlist<int> list_1(-1);
+	list_1.AddTail(int_massive, 10);
 	list_1.DeleteHead();
 	list_1.DeleteTail();
-	n = 16;
-	std::string* del_str_massive = new std::string[n];
-	for (int i = 10; i < n; i++)
-		del_str_massive[i] = std::to_string(i);
-	list_1.DeleteValue(del_str_massive, n);
 	std::cout << list_1;
 	std::cout << "____________________________________\n";
-	n = 3;
-	std::string* change = new std::string[n];
-	std::string* new_string = new std::string[n];
-	for (int i = 0; i < n; i++)
-		change[i] = std::to_string(i + 16);
-	for (int i = 0; i < n; i++)
-		new_string[i] = std::to_string(i + 19);
-
-	list_1.ChangeValue(change, new_string,  n);
+	int* delete_value = new int[3];
+	for (int i = 0; i < 3; i++)
+	{
+		delete_value[i] = i + 2;
+	}
+	list_1.DeleteValue(delete_value, 3);
+	std::cout << list_1;
+	std::cout << "____________________________________\n";;
+	int* new_value = new int[4];
+	int* index = new int[4];
+	for (int i = 0; i < 4; i++)
+	{
+		new_value[i] = i + 2;
+		index[i] = i + 2;
+	}
+	list_1.ChangeValue(index, new_value, 4);
 	std::cout << list_1;
 	std::cout << "____________________________________\n";
+	int* index_1 = new int[2];
+	for (int i = 0; i < 2; i++)
+		index_1[i] = i + 2;
+	int* search_value = list_1.SearchValue(index, 2);
+	for (int i = 0; i < 2; i++)
+		std::cout << search_value[i] << " ";
+	std::cout << "\n";
 
 }
