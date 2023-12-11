4e34ce0333688223d07b84f7b9c7d09be35a3e09
Nekbest <139134738+Nekbest@users.noreply.github.com>
2023-12-11 23:36:34 +0300

    Update BinaryTree.cpp

--- BinaryTrree/BinaryTree.cpp
+++ BinaryTrree/BinaryTree.cpp
@@ -1,4 +1,6 @@
 #include "BinaryTree.h"
+#include "string"
+#include<windows.h>
 
 void Init(TreeNode*& root)
 {
@@ -59,22 +61,16 @@ void Copy(TreeNode* root, TreeNode* new_root)
 		new_root = nullptr;
 }
 
-void DetacheLeftSubtree(TreeNode* root, TreeNode*& subtree)
+void DetacheLeftSubtree(TreeNode* root)
 {
-	if (root != nullptr)
-	{
-		subtree = root->left;
+	if (!IsEmpty(root))
 		root->left = nullptr;
-	}
 }
 
-void DetacheRightSubtree(TreeNode* root, TreeNode*& subtree)
+void DetacheRightSubtree(TreeNode* root)
 {
-	if (root != nullptr)
-	{
-		subtree = root->right;
+	if (IsEmpty(root))
 		root->right = nullptr;
-	}
 }
 
 void PrefOrder(TreeNode* root)
@@ -107,3 +103,63 @@ void PostOrder(TreeNode* root)
 	}
 }
 
+int MaxElement(TreeNode* root, int max_element)
+{
+	int value_1 = 0;
+	int value_2 = 0;
+	if (!IsEmpty(root))
+	{
+		if (root->element > max_element)
+			max_element = root->element;
+		value_1 = MaxElement(root->left, max_element);
+		value_2 = MaxElement(root->right, max_element);
+	}
+	else
+		return max_element;
+	if (value_1 >= value_2)
+		return value_1;
+	else
+		return value_2;
+}
+
+int CounterMaxElement(TreeNode* root, int max_element, int counter_max_element, int access)
+{
+	if (!IsEmpty(root))
+	{
+		if (access == 1)
+			max_element = MaxElement(root);
+		counter_max_element += CounterMaxElement(root->left, max_element, 0, 0);
+		counter_max_element += CounterMaxElement(root->right, max_element, 0, 0);
+	}
+	if (!IsEmpty(root) && max_element == root->element)
+		return counter_max_element + 1;
+	else
+		return counter_max_element;
+
+}
+
+void Print(TreeNode* root, int level, size_t len)
+{
+	if (!IsEmpty(root))
+	{
+		if (level == 0)
+			len =  std::to_string(MaxElement(root)).length();
+		Print(root->left, level + 1, len * 2);
+		for (int i = 0; i < level; i++)
+			std::cout << "    ";
+		if (level != 0)
+		{
+			for (int i = 0; i < len; i++)
+				std::cout << " ";
+			std::cout << root->element << "\n";
+		}
+		else
+		{
+			HANDLE hConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+			SetConsoleTextAttribute(hConsoleHandle, FOREGROUND_BLUE);
+			std::cout << root->element << "\n";
+			SetConsoleTextAttribute(hConsoleHandle, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		}
+		Print(root->right, level + 1, len * 2);
+	}
+}
