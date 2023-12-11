10d464295599796552eedb1f27169e95a1aebe03
Nekbest <139134738+Nekbest@users.noreply.github.com>
2023-12-11 23:36:08 +0300

    Update BinaryTree.h

--- BinaryTrree/BinaryTree.h
+++ BinaryTrree/BinaryTree.h
@@ -16,8 +16,12 @@ bool IsEmpty(TreeNode* root);
 bool AttachLeftSubTree(TreeNode* root, TreeNode* subtree);
 bool AttachRightSubTree(TreeNode* root, TreeNode* subtree);
 void Copy(TreeNode* root, TreeNode* new_root);
-void DetacheLeftSubtree(TreeNode* root, TreeNode*& subtree);
-void DetacheRightSubtree(TreeNode* root, TreeNode*& subtree);
+void DetacheLeftSubtree(TreeNode* root);
+void DetacheRightSubtree(TreeNode* root);
 void PrefOrder(TreeNode* root);
 void InOrder(TreeNode* root);
 void PostOrder(TreeNode* root);
+int MaxElement(TreeNode* root, int max_element = 0);
+int CounterMaxElement(TreeNode* root, int max_element = 0, int counter_max_element = 0, int access = 1);
+void Print(TreeNode* root, int level = 0, size_t len = 0);
+
