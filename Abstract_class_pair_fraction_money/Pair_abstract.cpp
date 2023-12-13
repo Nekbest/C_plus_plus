85a2b88d23c01a46d253220926335f3f781cd884
Nekbest <139134738+Nekbest@users.noreply.github.com>
2023-12-13 23:59:20 +0300

    Create Pair_abstract.cpp

---
+++ Abstract_class_pair_fraction_money/Pair_abstract.cpp
@@ -0,0 +1,44 @@
+#include "Pair_abstract.h"
+
+Pair::Pair()
+{
+	value_1 = 0;
+	value_2 = 0;
+}
+
+Pair::Pair(int val_1, int val_2)
+{
+	value_1 = val_1;
+	value_2 = val_2;
+}
+
+int Pair::get_value_1()
+{
+	return value_1;
+}
+
+int Pair::get_value_2()
+{
+	return value_2;
+}
+
+void Pair::set_value_1(int val_1)
+{
+
+	value_1 = val_1;
+}
+
+void Pair::set_value_2(int val_2)
+{
+	value_2 = val_2;
+}
+
+bool Pair::operator ==(Pair* other_pair)
+{
+	return ((value_1 == other_pair->get_value_1()) && value_2 == other_pair->get_value_2());
+}
+
+bool Pair::operator !=(Pair* other_pair)
+{
+	return !(this == other_pair);
+}
